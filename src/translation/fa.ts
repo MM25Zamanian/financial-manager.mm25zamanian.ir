2885e5c80834733a092e1b0e413d7149c5402121
S. MohammadMahdi Zamanian <76493524+MM25Zamanian@users.noreply.github.com>
2022-06-21 02:50:17 +0430

    Feat/internationalization (#2)
    * feat(internationalization): en translation
    
    * feat(internationalization): fa
    translation

---
+++ src/translation/fa.ts
@@ -0,0 +1,43 @@
+import type {DefaultTranslation} from '@shoelace-style/localize';
+
+const translation: DefaultTranslation = {
+  $code: 'fa',
+  $name: 'فارسی',
+  $dir: 'rtl',
+
+  $money: '﷼',
+  $title: 'مدیر مالی',
+
+  home: 'خانه',
+  about: 'درباره',
+  expenses: 'هزینه',
+  income: 'درآمد',
+  ok: 'تایید',
+  cancel: 'لغو',
+  balance: 'موازنه',
+  removing: 'در حال حذف ...',
+  delete: 'حذف',
+  edit: 'ویرایش',
+  registering_operations: 'در حال ثبت عملیات ...',
+  the_operation_was_successful: 'عملیات موفقیت آمیز بود',
+  operation_description: 'توضیحات عملیات',
+  operation_value: 'مقدار عملیات',
+  operation_category: 'دسته بندی عملیات',
+  record_operations: 'ثبت عملیات',
+  enter_a_description_of_this_operation: 'توضیحات این عملیات را وارد کنید',
+  enter_the_value_of_this_operation: 'مقدار این عملیات را وارد کنید',
+
+  salary: 'حقوق',
+  awards: 'جوایز',
+  coupons: 'کوپن ها',
+  sale: 'فروش',
+  grants: 'کمک های مالی',
+  other: 'دیگر',
+  shopping: 'خريد كردن',
+  education: 'تحصیلات',
+  gift: 'هدیه',
+  book: 'کتاب',
+  social: 'اجتماعی',
+};
+
+export default translation;
