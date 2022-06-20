83b9419dd28ee0331c2bd6751bc57d07e707b0d0
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2022-06-21 02:48:35 +0430

    feat(internationalization): en translation

---
+++ src/translation/en.ts
@@ -0,0 +1,43 @@
+import type {DefaultTranslation} from '@shoelace-style/localize';
+
+const translation: DefaultTranslation = {
+  $code: 'en',
+  $name: 'English',
+  $dir: 'ltr',
+
+  $money: 'US$',
+  $title: 'Financial Manager',
+
+  home: 'Home',
+  about: 'About',
+  expenses: 'Expenses',
+  income: 'Income',
+  ok: 'Ok',
+  cancel: 'Cancel',
+  balance: 'Balance',
+  removing: 'Removing ...',
+  delete: 'Delete',
+  edit: 'Edit',
+  registering_operations: 'Registering Operations ...',
+  the_operation_was_successful: 'The Operation Was Successful',
+  operation_description: 'Operation Description',
+  operation_value: 'Operation Value',
+  operation_category: 'Operation Category',
+  record_operations: 'Record Operations',
+  enter_a_description_of_this_operation: 'Enter A Description Of This Operation',
+  enter_the_value_of_this_operation: 'Enter The Value Of This Operation',
+
+  salary: 'Salary',
+  awards: 'Awards',
+  coupons: 'Coupons',
+  sale: 'Sale',
+  grants: 'Grants',
+  other: 'Other',
+  shopping: 'Shopping',
+  education: 'Education',
+  gift: 'Gift',
+  book: 'Book',
+  social: 'Social',
+};
+
+export default translation;
